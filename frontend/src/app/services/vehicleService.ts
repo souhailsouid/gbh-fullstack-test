6b64873ab98d4736ecffa1984f64f90d49dd20eb
souhail.souid.externe <souhail.souid.externe@emeria.eu>
2024-12-12 23:19:42 +0100

    feat: integrate centralized error handling  using handleAxiosError

--- frontend/src/app/services/vehicleService.ts
+++ frontend/src/app/services/vehicleService.ts
@@ -1,15 +1,25 @@
 import apiClient from './apiClient';
 import { Vehicle } from '../interfaces/Vehicles.interface';
-import {  cleanFilters } from '../utils/cleanFilters';
-
+import { cleanFilters } from '../utils/cleanFilters';
+import { handleAxiosError } from '../utils/errorHandler';
 
 export async function fetchVehicleById(id: string): Promise<Vehicle> {
-  const { data } = await apiClient.get(`/vehicles/${id}`);
-  return data;
+  try {
+    const { data } = await apiClient.get(`/vehicles/${id}`);
+    return data;
+  } catch (error) {
+    const message = handleAxiosError(error);
+    throw new Error(message); 
+  }
 }
 
 export async function fetchVehicles(filters: Record<string, string | number | undefined>) {
-     const filter = cleanFilters(filters);
+  try {
+    const filter = cleanFilters(filters);
     const { data } = await apiClient.get('/vehicles', { params: filter });
     return data;
+  } catch (error) {
+    const message = handleAxiosError(error);
+    throw new Error(message); 
+  }
 }
